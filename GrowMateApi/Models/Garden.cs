7498a9bb2b9c9a2d6209b00bdc1246fa45d82d50
Dawid Studzizba <studzizba.dawid@gmail.com>
2024-09-13 15:31:10 +0200

    Define database models

---
+++ GrowMateApi/Models/Garden.cs
@@ -0,0 +1,9 @@
+﻿public class Garden
+{
+	public string Id { get; set; }
+	public string UserId { get; set; }
+	public string Name { get; set; }
+	public List<Plant> Plants { get; set; }
+	public string Location { get; set; }
+	public SoilParameters Soil { get; set; }
+}
