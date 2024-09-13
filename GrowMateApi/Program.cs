cb74badd833b1f0d8fc66b2bf2d0d4cf9e7d8c69
Dawid Studzizba <studzizba.dawid@gmail.com>
2024-09-13 18:00:26 +0200

    Adjust Program.cs to populate the database

--- GrowMateApi/Program.cs
+++ GrowMateApi/Program.cs
@@ -33,6 +33,12 @@ namespace GrowMateApi
 
 			var app = builder.Build();
 
+			using (var scope = app.Services.CreateScope())
+			{
+				var database = scope.ServiceProvider.GetRequiredService<IMongoDatabase>();
+				SeedData.Initialize(database).Wait();
+			}
+
 			// Configure the HTTP request pipeline.
 			if (app.Environment.IsDevelopment())
 			{
