b5542e7bc8a5a1a6c5f4beb4050043f03c174e51
Dawid Studzizba <studzizba.dawid@gmail.com>
2024-11-04 21:59:46 +0100

    Add plant logs

---
+++ GrowMateApi/Models/PlantTrackingLog.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace GrowMateApi.Models
+{
+	public class PlantTrackingLog
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+		public string PlantId { get; set; }
+		public DateTime EventDate { get; set; }
+		public string EventType { get; set; }
+		public string? Notes { get; set; }
+		public string? PhotoUrl { get; set; }
+	}
+}
